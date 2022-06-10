d452f012503e9fcb467279da4ae4500833570831
razakadegoke <34461459+razakadegoke@users.noreply.github.com>
2022-06-10 17:20:52 -0400

    Update puzzle.js

--- puzzle.js
+++ puzzle.js
@@ -1,6 +1,6 @@
 const variablesReferenceTest = () => {
     const x = { attribute1: 1, attribute2: 2 };
-    const y = { attribute1: 1, attribute2: 2 };;
+    const y = { attribute1: 1, attribute2: 2 };
     x.attribute1 = 3;
 
     //Modify the code above so we reach the "throw 'False'" statement
